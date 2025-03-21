2a8e1edbf8ced1a862e65ae18e4273c5fe9258b8
Nazif96 <afolabinazif96@gmail.com>
2025-03-21 15:20:40 +0100

    Update dash

--- Main.py
+++ Main.py
@@ -1,10 +1,10 @@
 import streamlit as st 
 import pandas as pd
 import numpy as np
-from numerizer import numerize
+from numerize import numerize
 import plotly.express as px
 import plotly.subplots as sp
 
-st.set_page_config(page_title="Streamlit App", page_icon=":bar_chart:", layout="wide")
+st.set_page_config(page_title="Business Analytics Dashbord", page_icon="🌍", layout="wide")
 st.subheader("📉 Business Analytics Dashbord") 
 
