8a17c56caef8dd0a6ac54e1271f3a8579612cf4f
Nazif96 <afolabinazif96@gmail.com>
2025-03-21 15:36:25 +0100

    Update

--- Main.py
+++ Main.py
@@ -8,3 +8,13 @@ import plotly.subplots as sp
 st.set_page_config(page_title="Business Analytics Dashbord", page_icon="🌍", layout="wide")
 st.subheader("📉 Business Analytics Dashbord") 
 
+# chargement CSS style
+with open("style.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+# chargement des données
+@st.cache
+df = pd.read_csv("Données/customers.csv")  # chargement des données
+
+df.head()
