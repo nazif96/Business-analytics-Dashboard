9cb486ed1c96053db7787f2a198704297caf9974
Nazif96 <afolabinazif96@gmail.com>
2025-03-21 19:06:48 +0100

    dashboard

--- Main.py
+++ Main.py
@@ -5,6 +5,7 @@ from numerize import numerize
 import plotly.express as px
 import plotly.subplots as sp
 import os 
+from streamlit_extras.metric_cards import style_metric_cards
 
 st.set_page_config(page_title="Business Analytics Dashbord", page_icon="🌍", layout="wide")
 st.subheader("📉 Business Analytics Dashbord") 
@@ -41,23 +42,26 @@ businessunit = st.sidebar.multiselect(
 df_selection = df.query("Department == @departement & Country == @pays & BusinessUnit == @businessunit")
 
 
-# Meilleurs analyses 
+# top analyses 
 
 def metrics():
-    from streamlit_extras.metric_cards import style_metric_cards 
-    st.subheader("📊 Meilleurs analyses")
+    from streamlit_extras.metric_cards import style_metric_cards
+    st.subheader("📊 Metrics")
     col1, col2, col3 = st.columns(3)
     
-    col1.metrics(label="Total Clients", value= df_selection.Gender.count(), delta="tout les clients")
+    col1.metric(label="Total Clients", value= df_selection.Gender.count(), delta="tout les clients")
     
-    col2.metrics(label="Total salaire annuel", value= f"{df_selection.AnnualSalary.sum():, .0f}", delta=df.AnnualSalary.median()) 
+    #col2.metric(label="Total salaire annuel", value= f"{df_selection.AnnualSalary.sum():,.0f}", delta=df.AnnualSalary.median()) 
+    col2.metric(label="Total salaire annuel", value=f"{df_selection.AnnualSalary.sum():,.0f}", delta=df.AnnualSalary.median())
+
     
-    col3.metrics(label= "Salaire Annuel", value = f"{df_selection.AnnualSalary.max()-df.AnnualSalary.min():, .0f}", delta= "échelle Salariale annuelle") 
+    col3.metric(label= "Salaire Annuel", value = f"{df_selection.AnnualSalary.max()-df.AnnualSalary.min():,.0f}", delta= "échelle Salariale annuelle") 
     
-    style_metric_cards(background_color="#121270", border_left_color="#FFD700", text_color="#f20045", box_shadow="3px")
+    style_metric_cards(background_color="#121270", border_left_color="#FFD700", border_color="#f20045", box_shadow="3px")
     
-    
-# ceer les div pour les graphiques
+  
+
+# créer les div pour les graphiques
 
 div1, div2 = st.columns(2) 
 # 📊 pie chart 
@@ -65,7 +69,7 @@ def pie_chart():
     with div1: 
         theme_plotly = None  
         fig = px.pie(df_selection, values='AnnualSalary', names='Department', title='Répartition des salaires par département')
-        fig.update_layout(legende_title ='Country', legend_y=0.9)
+        fig.update_layout(legend_title ='Country', legend_y=0.9)
         fig.update_traces(textinfo ='percent+label', textposition= 'inside')
         st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
         
@@ -74,7 +78,7 @@ def bar_chart():
     theme_plotly = None 
     with div2:
         theme_plotly = None
-        fig = px.bar(df_selection, x='Department', y='AnnualSalary', text_auto= '.2s', title=' ')
+        fig = px.bar(df_selection, x='Department', y='AnnualSalary', text_auto= '.2s', title='Salaires par Département')
         fig.update_traces(textfont_size= 16, textangle=0, textposition='outside', cliponaxis=False)
         st.plotly_chart(fig, use_container_width=True, theme="streamlit")
         
